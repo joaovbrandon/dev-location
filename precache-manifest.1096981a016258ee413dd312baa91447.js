4a56aa258ccb8469f58428d3e875ee3b7a6277be
joaaoeu <hello@joaao.eu>
2019-04-03 00:39:33 -0300

    Updates

---
+++ precache-manifest.1096981a016258ee413dd312baa91447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec2a082e0f893c351d46",
+    "url": "/dev-location/static/js/runtime~main.cfc5adcb.js"
+  },
+  {
+    "revision": "7e459f2f0647c5057898",
+    "url": "/dev-location/static/js/main.278830a4.chunk.js"
+  },
+  {
+    "revision": "ef2160c840b862bd5942",
+    "url": "/dev-location/static/js/2.8b129479.chunk.js"
+  },
+  {
+    "revision": "ef2160c840b862bd5942",
+    "url": "/dev-location/static/css/2.c9397e6b.chunk.css"
+  },
+  {
+    "revision": "75f6b2e415a2a8245a3e9d63461ace18",
+    "url": "/dev-location/index.html"
+  }
+];
